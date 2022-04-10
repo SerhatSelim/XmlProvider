7b56721c771ec191da1c03d94407387e7e09d789
Serhat Selim <serhatselim@hotmail.com.tr>
2022-04-10 14:53:11 +0300

    xsd validator

---
+++ XsdValidator/Example.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace XsdValidator
+{
+    public class Example
+    {
+
+        public Example()
+        {
+            string xsdMarkup =
+                                @"<xsd:schema xmlns:xsd='http://www.w3.org/2001/XMLSchema'>
+                                   <xsd:element name='Root'>
+                                    <xsd:complexType>
+                                     <xsd:sequence>
+                                      <xsd:element name='Child1' minOccurs='1' maxOccurs='1'/>
+                                      <xsd:element name='Child2' minOccurs='1' maxOccurs='1'/>
+                                     </xsd:sequence>
+                                    </xsd:complexType>
+                                   </xsd:element>
+                                  </xsd:schema>";
+
+            XmlSchemaSet schemas = new XmlSchemaSet();
+            schemas.Add("", XmlReader.Create(new StringReader(xsdMarkup)));
+
+            XDocument doc1 = new XDocument(
+                new XElement("Root",
+                    new XElement("Child1", "content1"),
+                    new XElement("Child2", "content1")
+                )
+            );
+
+            XDocument doc2 = new XDocument(
+                new XElement("Root",
+                    new XElement("Child1", "content1"),
+                    new XElement("Child3", "content1")
+                )
+            );
+
+            Console.WriteLine("Validating doc1");
+            bool errors = false;
+            doc1.Validate(schemas, (o, e) =>
+            {
+                Console.WriteLine("{0}", e.Message);
+                errors = true;
+            });
+            Console.WriteLine("doc1 {0}", errors ? "did not validate" : "validated");
+
+            Console.WriteLine();
+            Console.WriteLine("Validating doc2");
+            errors = false;
+            doc2.Validate(schemas, (o, e) =>
+            {
+                Console.WriteLine("{0}", e.Message);
+                errors = true;
+            });
+            Console.WriteLine("doc2 {0}", errors ? "did not validate" : "validated");
+        }
+
+       
+    }
+}
